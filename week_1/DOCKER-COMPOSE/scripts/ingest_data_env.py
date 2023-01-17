9d704439d6faa2e7f192f81a5caad437133419be
Alexey Mayer <alexey_mayer@epam.com>
2023-01-17 18:13:06 +0300

    Week_1 add docker-compose for postgres, pgadmin and csv data injection

---
+++ week_1/DOCKER-COMPOSE/scripts/ingest_data_env.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from sqlalchemy import create_engine
+from time import time
+import os
+import pandas as pd
+
+def main():
+    
+    csv_name = 'output.csv'
+    os.system(f"wget {url} -O {csv_name}")
+    
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+
+    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
+    df = next(df_iter)
+
+
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+    df.to_sql(name=table_name, con=engine, if_exists='append')
+
+
+    while True:
+        t_start = time()
+        
+        df = next(df_iter)
+        
+        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+        
+        df.to_sql(name=table_name, con=engine, if_exists='append')
+        
+        t_end = time()
+        
+        print('inserted another chunk..., took %.3f second' % (t_end - t_start))
+
+if __name__ == '__main__':
+    user = os.getenv('PG_USER')
+    password = os.getenv('PG_PASSWORD')
+    host = os.getenv('PG_HOST')
+    port = os.getenv('PG_PORT')
+    db = os.getenv('PG_DB')
+    table_name = os.getenv('PG_TABLE_NAME')
+    url = os.getenv('SQL_FILE_URL')
+
+    main()
